983da7837ddc7efc39c5650053d127151e2cccfc
haiqal-uthm <di220125@student.uthm.edu.my>
2025-05-16 03:38:19 +0800

    Update firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -2,8 +2,20 @@
 
 return [
 'credentials' => [
-    'json' => json_decode(env('FIREBASE_CREDENTIALS'), true),
-],
+        'json' => [
+            'type'                        => env('FIREBASE_TYPE'),
+            'project_id'                  => env('FIREBASE_PROJECT_ID'),
+            'private_key_id'              => env('FIREBASE_PRIVATE_KEY_ID'),
+            // Turn the “\n” sequences back into real newlines:
+            'private_key'                 => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+            'client_email'                => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id'                   => env('FIREBASE_CLIENT_ID'),
+            'auth_uri'                    => 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri'                   => 'https://oauth2.googleapis.com/token',
+            'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_x509_cert_url'        => 'https://www.googleapis.com/robot/v1/metadata/x509/' . rawurlencode(env('FIREBASE_CLIENT_EMAIL')),
+        ],
+    ],
 
 
     'database' => [
