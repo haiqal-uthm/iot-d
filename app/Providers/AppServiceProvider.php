b0802c1e72e5b116dd1c669166388ce2d7e8c68c
haiqal-uthm <di220125@student.uthm.edu.my>
2025-05-16 15:27:41 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,9 +7,16 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    public function boot()
+    {
+        VerifyCsrfToken::except([
+            '/vibration-log', // API kau tadi
+        ]);
+
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+    }
     public function register(): void
     {
         //
