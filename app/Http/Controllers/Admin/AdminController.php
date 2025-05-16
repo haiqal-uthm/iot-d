2a6c1600db2d0099f4cdaeac29cc151863aa8f52
haiqal-uthm <di220125@student.uthm.edu.my>
2025-05-16 15:32:30 +0800

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -32,7 +32,7 @@ class AdminController extends Controller
     public function index()
     {
         // Retrieve data from Firebase
-        $weather = $this->firebaseService->getWeatherData();
+        //$weather = $this->firebaseService->getWeatherData();
         $totalDurian = $this->firebaseService->getDurianCount();
         $totalDevice = Device::count();
         $totalOrchards = Orchard::count();
