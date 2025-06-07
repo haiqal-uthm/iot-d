d6f43e85fe156b4537a9ce1a0dddd5d7f1ac24a9
haiqal-uthm <di220125@student.uthm.edu.my>
2025-06-07 23:44:33 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,8 @@ use Illuminate\Support\Facades\Route;
 //IoT Backend Things
 Route::post('/harvest-log', [HarvestController::class, 'store']);
 Route::post('/vibration-log', [VibrationLogController::class, 'store']);
+Route::get('/test', function () {
+    return response()->json(['message' => 'API works!']);
+});
+
 
