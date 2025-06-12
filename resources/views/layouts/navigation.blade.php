f7b913aae5122bdfb422b51d2d298140c662fcb9
eqalls <di220125@student.uthm.edu.my>
2025-06-12 22:33:35 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -165,6 +165,16 @@ s    class="bg-white dark:bg-gray-800 border-r border-gray-100 dark:border-gray-
                             </div>
                         </x-responsive-nav-link>
                     @elseif(Auth::user()->role === 'manager')
+                    <x-responsive-nav-link :href="route('manager.dashboard')" :active="request()->routeIs('manager.dashboard')"
+                            class="block px-3 py-2 rounded-md text-base font-medium">
+                            <div class="flex items-center">
+                                <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                                </svg>
+                                {{ __('Dashboard') }}
+                            </div>
+                        </x-responsive-nav-link>
                         <x-responsive-nav-link :href="route('manager.durian-fall.index')" :active="request()->routeIs('manager.durian-fall.index')">
                             <div class="flex items-center">
                                 <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
