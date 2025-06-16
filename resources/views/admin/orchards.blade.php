724ad2a6d352f3dc7d486df21d31f9b307e6dfc1
eqalls <di220125@student.uthm.edu.my>
2025-06-17 05:59:18 +0800

    Update orchards.blade.php

--- resources/views/admin/orchards.blade.php
+++ resources/views/admin/orchards.blade.php
@@ -121,7 +121,7 @@
                     <div class="orchard-card-footer">
                         <div class="flex space-x-2">
                             <button
-                                onclick="resetFirebaseCounter('{{ $orchard->id }}')"
+                                onclick="resetVibrationCount('{{ $orchard->id }}')"
                                 class="btn-primary flex-1 flex items-center justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor">
