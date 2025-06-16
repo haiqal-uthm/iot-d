30aff155e14ff5c7b8f4db51f862c0d4386074a1
eqalls <di220125@student.uthm.edu.my>
2025-06-17 04:08:03 +0800

    Update edit.blade.php

--- resources/views/orchards/edit.blade.php
+++ resources/views/orchards/edit.blade.php
@@ -17,7 +17,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
-                    <form action="{{ route(admin.orchards.update', $orchard->id) }}" method="POST" class="space-y-6">
+                    <form action="{{ route('admin.orchards.update', $orchard->id) }}" method="POST" class="space-y-6">
                         @csrf
                         @method('PUT')
 
